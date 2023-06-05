85397b349e72263acf5f4bdbe065b66df470343f
Johnny Martinez <121833123+jjm469@users.noreply.github.com>
2023-06-04 21:15:17 -0400

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,17 @@
+#========================================================================
+# Package containing all scripts used in dataset generation scripts 
+#========================================================================
+
+"""
+split_string is a function shortens long strings to have a max of 80 
+characters per line. Does not split words.
+"""
+def split_string(string):
+    if len(string) <= 80:
+        return string
+    else:
+        split_idx = string[:80].rfind(' ')
+        if split_idx == -1:
+            split_idx = 80
+        return string[:split_idx] + '\n' + split_string(string[split_idx+1:])
+
